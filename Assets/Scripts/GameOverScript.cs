b484b73c865f5c73c694e0172f4c17e2acd445b5
maniferrari <maniferrari@gmail.com>
2016-06-21 09:01:27 -0700

    added logo etc

--- Assets/Scripts/GameOverScript.cs
+++ Assets/Scripts/GameOverScript.cs
@@ -5,15 +5,19 @@ using UnityEngine.UI;
 public class GameOverScript : MonoBehaviour {
 	ScoreAbsorber scoreabsorber;
 	Text text;
+	int highscore;
 
 	// Use this for initialization
 	void Start () {
 		scoreabsorber = FindObjectOfType<ScoreAbsorber>();
+		highscore = PlayerPrefs.GetInt ("highscore", highscore);
+		Debug.Log("High score is "+ highscore);
 		text = GetComponent<Text>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		
 		text.text = "Score : "+scoreabsorber.score;
 	
 	}
